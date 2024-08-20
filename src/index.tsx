4640494dff468980e4bc16dc3f8911743997c61b
caoyi <caoyi@xizi-ai.com>
2024-08-20 17:36:46 +0800

    update: 页面拆分

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,48 @@
-import {  FC } from 'react';
+import {  FC, useEffect, useState } from 'react';
 import Panel from './panel'
 import { EventSourceMessage } from '@microsoft/fetch-event-source';
-
+import StreamFetcher from "./fetch";
+import { getStreamMap } from "./utils";
 export interface FetchStreamProps {
-
   /** 自定义 StreamItem 组件 */
   CustomStreamItem ?: FC<{event: EventSourceMessage}>
 }
 
+const fetcher = new StreamFetcher()
+
 const FetchStream: FC<FetchStreamProps> = (props) => {
-  return <Panel { ...props} ></Panel>
+  const { CustomStreamItem, } = props
+  const { start } = fetcher
+  const initMap = getStreamMap()
+  const [streamMap, setStreamMap] = useState(initMap)
+
+  // 设置流的内容
+  const setStreamMapFn = (event: EventSourceMessage) => {
+    setStreamMap(prevStreamMap => {
+      const newStreamMap = getStreamMap(prevStreamMap)
+      newStreamMap.set(event.id, event)
+      return newStreamMap
+    })
+  }
+  const create = async () => {
+    start('http://localhost:3000/stream', {
+      onopen: async (event) => {
+        console.log(event, 'event');
+      },
+      onmessage(event) {
+        console.log(event, 'ev');
+        // 让外部传入的props.customMessage返回一个新的event, 这样可以让外界可以控制数据
+        // setStreamMapFn(props.customMessage)
+        setStreamMapFn(event)
+      },
+    })
+  }
+
+  useEffect(() => {
+    create()
+  }, [])
+  const streamList = Array.from(streamMap.values())
+  return <Panel streamList={streamList} CustomStreamItem={CustomStreamItem}  ></Panel>
 };
 
 export default (FetchStream);
--- src/panel.tsx
+++ src/panel.tsx
@@ -1,44 +1,15 @@
-import { FC, Fragment, memo, useEffect, useState } from "react";
-import StreamFetcher from "./fetch";
-import { getStreamMap } from "./utils";
+import { FC, Fragment, memo } from "react";
+
 import { EventSourceMessage } from "@microsoft/fetch-event-source";
-import { FetchStreamProps } from ".";
 
-const fetcher = new StreamFetcher()
+interface PanelProps {
+  streamList: EventSourceMessage[]
+  CustomStreamItem ?: FC<{event: EventSourceMessage}>
+}
 
-const Panel : FC<FetchStreamProps>= (props) => {
-  const { CustomStreamItem } = props
-  const { start } = fetcher
-  const initMap = getStreamMap()
-  const [streamMap, setStreamMap] = useState(initMap)
-
-  // 设置流的内容
-  const setStreamMapFn = (event: EventSourceMessage) => {
-    setStreamMap(prevStreamMap => {
-      const newStreamMap = getStreamMap(prevStreamMap)
-      newStreamMap.set(event.id, event)
-      return newStreamMap
-    })
-  }
-  const create = async () => {
-    start('http://localhost:3000/stream', {
-      onopen: async (event) => {
-        console.log(event, 'event');
-      },
-      onmessage(event) {
-        console.log(event, 'ev');
-        // 让外部传入的props.customMessage返回一个新的event, 这样可以让外界可以控制数据
-        // setStreamMapFn(props.customMessage)
-        setStreamMapFn(event)
-      },
-    })
-  }
-
-  useEffect(() => {
-    create()
-  }, [])
+const Panel : FC<PanelProps>= (props) => {
+  const { CustomStreamItem, streamList } = props
   const StreamItem = ({event}: {event: EventSourceMessage}) => <span>{event.data}</span>
-  const streamList = Array.from(streamMap.values())
 
   return (
     <>
