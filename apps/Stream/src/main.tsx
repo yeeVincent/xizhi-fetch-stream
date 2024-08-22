4e51bfb312a807d6ce65db4cd459c7329ea53cea
caoyi <caoyi@xizi-ai.com>
2024-08-22 20:39:19 +0800

    add: 启动第一个项目

--- apps/Stream/src/main.tsx
+++ apps/Stream/src/main.tsx
@@ -3,4 +3,6 @@ import React from 'react';
 import FetchStream from '../src/index';
 console.log('加载main');
 
-createRoot(document.getElementById('root')!).render(<FetchStream></FetchStream>)
+createRoot(document.getElementById('root')!).render(<>
+<FetchStream></FetchStream>
+</>)
