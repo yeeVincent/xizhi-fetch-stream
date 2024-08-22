878e6d4da3f3a9934ffe81b24c6411d949503528
caoyi <caoyi@xizi-ai.com>
2024-08-22 22:23:46 +0800

    add: turbo重构项目

--- apps/Template/src/main.tsx
+++ apps/Template/src/main.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import FetchStream from '../src/index';
+import FetchStream from '@repo/stream';
 console.log('加载main');
 
 createRoot(document.getElementById('root')!).render(<>
+123
 <FetchStream></FetchStream>
 </>)
